ce43edd5c411d3b624bc61f912961ec70dc75807
Gabriel Freire <gabriel.freire@eu.equinix.com>
2021-03-18 19:28:24 +0000

    using css selectors

--- src/app/category-list.component.ts
+++ src/app/category-list.component.ts
@@ -1,14 +1,29 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: "mw-category-list",
+  selector: 'mw-category-list',
   template: `
     <span class="label" *ngFor="let category of categories">
       {{ category }}
     </span>
   `,
   styles: [
-    ``
+    `
+        span {
+          display: inline-block;
+          margin-right: 4px;
+          margin-bottom: 4px;
+        }
+        .label {
+          background-color: #f7e6ff;
+          color: #2d00ff;
+          border-radius: 4px;
+          padding: 2px 6px;
+          font-style: italic;
+          width: max-content;
+
+        }
+    `
   ]
 })
 export class CategoryListComponent {
